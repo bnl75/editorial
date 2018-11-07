e3da6c3f0c3278d63d11eb8adf443ced9181df66
bnl75 <diegoartu@hotmail.com>
2018-11-07 14:39:17 -0600

    Este es mi primer commit y agregamos css, js, img y html

---
+++ img/Libros_files/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$('.sidenav').sidenav();
+	$('.carousel').carousel();
+	$('.parallax').parallax();
+	$('.materialboxed').materialbox();
+	$('input#input_text, textarea#textarea2').characterCounter();
+});
---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('.sidenav').sidenav();
+	$('.carousel').carousel();
+	$('.parallax').parallax();
+	$('.materialboxed').materialbox();
+	$('input#input_text, textarea#textarea2').characterCounter();
+	$(".dropdown-trigger").dropdown();
+	$('.slider').slider();
+});
